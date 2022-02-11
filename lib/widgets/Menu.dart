109e691655751080a13a44e6baa1e7b82fe83356
Akash <akash.sivanandan@protonmail.coim>
2022-02-11 11:57:55 +0530

    kdfjh

--- lib/widgets/Menu.dart
+++ lib/widgets/Menu.dart
@@ -3,35 +3,6 @@ import 'Time.dart';
 import 'ResetButton.dart';
 import 'Move.dart';
 
-// class Menu extends StatelessWidget {
-//   // Function reset;
-// VoidCallback reset;
-// int move;
-// int secondsPassed;
-// var size;
-
-//   Menu(this.reset, this.move, this.secondsPassed, this.size);
-
-//   @override
-//   Widget build(BuildContext context) {
-// return Container(
-//   height: size.height * 0.10,
-//   child: Row(
-//     mainAxisAlignment: MainAxisAlignment.spaceAround,
-//     crossAxisAlignment: CrossAxisAlignment.start,
-//     children: <Widget>[
-//       ResetButton(
-//         reset: reset,
-//         text: 'Reset',
-//       ),
-//       Move(move),
-//       Time(secondsPassed),
-//     ],
-//   ),
-// );
-//   }
-// }
-
 class Menu extends StatelessWidget {
   Menu(
       {Key? key,
