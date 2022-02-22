90cdd191a0f46fca214d48c64795deca0b491071
Akash <akash.sivanandan@protonmail.coim>
2022-02-22 15:35:55 +0530

    Made a button for changing the theme of the puzzle

--- lib/widgets/ChangeThemeButton.dart
+++ lib/widgets/ChangeThemeButton.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class ChangeThemeButton extends StatelessWidget {
+  const ChangeThemeButton({
+    Key? key,
+    required this.next,
+  }) : super(key: key);
+
+  final VoidCallback next;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(30.0)),
+          )),
+      onPressed: next,
+      child: Text(
+        "Change theme",
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
