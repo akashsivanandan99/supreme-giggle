0b420c1ff6ab8a6263e914b3ea102b271fb62e24
Akash <akash.sivanandan@protonmail.coim>
2022-02-22 15:35:16 +0530

    No specific changes

--- lib/themes/color_palettes.dart
+++ lib/themes/color_palettes.dart
@@ -10,10 +10,17 @@ const List<Color> retro_colors = [
 const List<Color> palette_2 = [
   Color.fromRGBO(243, 129, 129, 1),
   Color.fromRGBO(252, 227, 138, 1),
-  Color.fromRGBO(234, 255, 208, 1),
+  Color.fromARGB(255, 234, 255, 208),
   Color.fromRGBO(149, 225, 211, 1),
 ];
 
+const List<Color> cyberpunk = [
+  Color.fromRGBO(0, 13, 107, 1),
+  Color.fromRGBO(156, 25, 224, 1),
+  Color.fromRGBO(255, 93, 162, 1),
+  Color.fromRGBO(153, 221, 204, 1),
+];
 
+const List palettes = [retro_colors, cyberpunk, palette_2];
 
 // var randomItem = (colors..shuffle()).first;
