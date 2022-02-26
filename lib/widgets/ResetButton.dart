0a26a3863d4cd26f84253ad7b61ca2582c209d86
Akash <akash.sivanandan@protonmail.coim>
2022-02-26 12:42:08 +0530

    Changed it from a button w text to an icon button

--- lib/widgets/ResetButton.dart
+++ lib/widgets/ResetButton.dart
@@ -3,29 +3,40 @@ import 'package:flutter/material.dart';
 class ResetButton extends StatelessWidget {
   const ResetButton({
     Key? key,
-    required this.text,
+    // required this.text,
     required this.reset,
   }) : super(key: key);
 
-  final String text;
+  // final String text;
   final VoidCallback reset;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: reset,
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.white),
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(30.0)),
-          )),
-      child: Text(
-        "Reset",
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-        ),
+      // style: ButtonStyle(
+      //     backgroundColor: MaterialStateProperty.all(Colors.white),
+      //     shape: MaterialStateProperty.all(
+      //       RoundedRectangleBorder(
+      //           borderRadius: new BorderRadius.circular(30.0)),
+      //     )),
+      style: ElevatedButton.styleFrom(
+        shape: CircleBorder(),
+        primary: Colors.white,
+        padding: EdgeInsets.all(10),
+      ),
+      // child: Text(
+      //   "Reset",
+      //   style: TextStyle(
+      //     color: Colors.black,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      // ),
+
+      child: Icon(
+        Icons.replay,
+        color: Colors.black,
+        size: 30,
       ),
     );
   }
